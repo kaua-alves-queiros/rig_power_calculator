ad77f7a9a9a3db252f593e1e9cd15fb348501d8a
kaua-alves-queiros <kaua.alves.queiros@hotmail.com>
2023-06-03 17:56:38 -0400

    created home page

--- lib/application.dart
+++ lib/application.dart
@@ -1,9 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/get_navigation.dart';
+import 'package:rig_power_calculator/page/home.dart';
 
 class Application extends StatelessWidget {
+  const Application({
+    super.key,
+  });
+
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp();
+    return GetMaterialApp(
+      initialRoute: HomePage.route,
+      getPages: [
+        GetPage(
+          name: HomePage.route,
+          page: () => const HomePage(),
+        ),
+      ],
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -2,5 +2,5 @@ import 'package:flutter/cupertino.dart';
 import 'package:rig_power_calculator/application.dart';
 
 void main() {
-  runApp(Application());
+  runApp(const Application());
 }
