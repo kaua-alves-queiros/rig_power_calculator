181c86c4cc2003c1b3d0f2d1fc1572fc4e601a66
kaua-alves-queiros <kaua.alves.queiros@hotmail.com>
2023-06-03 17:47:23 -0400

    created application and remove test file, and remove trash form main file

---
+++ lib/application.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:get/get_navigation/get_navigation.dart';
+
+class Application extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp();
+  }
+}
