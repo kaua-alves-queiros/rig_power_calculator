128f1ab35504088bb3c02d212245ec48ba7aacd4
kaua-alves-queiros <kaua.alves.queiros@hotmail.com>
2023-06-03 20:45:42 -0400

    remove firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:get_storage/get_storage.dart';
 import 'package:rig_power_calculator/application.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
 
 void main() async {
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-
   await GetStorage.init();
   runApp(const Application());
 }
