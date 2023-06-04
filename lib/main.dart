daca84d796dfbbedb4c262ce005bd8bef5bea572
kaua-alves-queiros <kaua.alves.queiros@hotmail.com>
2023-06-03 20:26:58 -0400

    added firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/cupertino.dart';
 import 'package:get_storage/get_storage.dart';
 import 'package:rig_power_calculator/application.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 void main() async {
+// ...
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   await GetStorage.init();
   runApp(const Application());
 }
