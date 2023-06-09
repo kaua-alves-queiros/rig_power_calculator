7712dc5bd2d55109037d4f25a9e0997af12aaa5b
kaua-alves-queiros <kaua.alves.queiros@hotmail.com>
2023-06-08 21:35:09 -0400

    created rigs controller

---
+++ lib/controller/rig.dart
@@ -0,0 +1,21 @@
+import 'package:get/get.dart';
+
+import '../model/rig.dart';
+import '../storage/Rigs.dart';
+
+class RigsController extends GetxController {
+  Rx<List<RigModel>>? rigs;
+
+  void loadRigs() {
+    rigs ??= Rx(List<RigModel>.empty());
+
+    rigs!.value = RigsStorage.all();
+  }
+
+  void editRig(String id) {}
+
+  void deleteRig(String id) {
+    RigsStorage.removeById(id);
+    loadRigs();
+  }
+}
