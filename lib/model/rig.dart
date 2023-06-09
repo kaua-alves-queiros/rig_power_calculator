9badf4c806c1319f9f53bd8c136b6ea447d2e00d
kaua-alves-queiros <kaua.alves.queiros@hotmail.com>
2023-06-08 21:33:26 -0400

    removed name and added a radom unique id basedin uuid

--- lib/model/rig.dart
+++ lib/model/rig.dart
@@ -1,11 +1,29 @@
 import 'device.dart';
 
 class RigModel {
-  String name;
+  String id;
   List<DeviceModel> devices;
 
   RigModel({
-    required this.name,
+    required this.id,
     required this.devices,
   });
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'id': id,
+      'devices': devices.map((x) => x.toMap()).toList(),
+    };
+  }
+
+  factory RigModel.fromMap(Map<String, dynamic> map) {
+    return RigModel(
+      id: map['id'] as String,
+      devices: List<DeviceModel>.from(
+        (List<Map<String, dynamic>>.from(map['devices'])).map<DeviceModel>(
+          (x) => DeviceModel.fromMap(x as Map<String, dynamic>),
+        ),
+      ),
+    );
+  }
 }
