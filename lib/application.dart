3604a24ceaede260169cc77b17ff9e5176c76987
kaua-alves-queiros <kaua.alves.queiros@hotmail.com>
2023-06-08 21:35:30 -0400

    added rigs page in application as initial route

--- lib/application.dart
+++ lib/application.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/get_navigation.dart';
 import 'package:rig_power_calculator/page/home.dart';
+import 'package:rig_power_calculator/page/rigs.dart';
 
 class Application extends StatelessWidget {
   const Application({
@@ -11,14 +12,18 @@ class Application extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       title: 'Rig Power Calculator',
-      initialRoute: HomePage.route,
+      initialRoute: RigsPage.route,
       themeMode: ThemeMode.system,
       darkTheme: ThemeData.dark(),
       theme: ThemeData.light(),
       getPages: [
         GetPage(
           name: HomePage.route,
-          page: () => HomePage(),
+          page: () => const HomePage(),
+        ),
+        GetPage(
+          name: RigsPage.route,
+          page: () => const RigsPage(),
         ),
       ],
     );
